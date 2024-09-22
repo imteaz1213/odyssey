5fe1d2fc7bbb1d13a8779b54125c51251af25c2c
imteaz1213 <mdimteaz567@gmail.com>
2024-09-22 14:38:25 +0600

    TakeLeaveActivity & MakeComplainActivity Added

---
+++ app/src/main/java/com/example/odyssey/MakeComplainActivity.java
@@ -0,0 +1,27 @@
+package com.example.odyssey;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.odyssey.databinding.ActivityMakeComplainBinding;
+
+public class MakeComplainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_complain);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/odyssey/TakeLeaveActivity.java
@@ -0,0 +1,29 @@
+package com.example.odyssey;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.odyssey.databinding.ActivityTakeLeaveBinding;
+
+public class TakeLeaveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_leave);
+
+
+
+    }
+
+}
