420d755f55eb910e9cab518e9bb1197832b76605
Nuzhat-Nasrum <nuzhatnasrum111.gmail.com>
2024-11-18 13:36:06 +0600

    logout button integreated

--- app/src/main/java/com/example/odyssey/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/odyssey/fragments/ProfileFragment.java
@@ -1,5 +1,8 @@
 package com.example.odyssey.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +10,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.odyssey.R;
+import com.example.odyssey.SigninActivity;
 
 public class ProfileFragment extends Fragment {
 
+    private Button logoutBtn;
+    private SharedPreferences sharedPreferences;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        logoutBtn = view.findViewById(R.id.logoutBtn);
+        sharedPreferences = requireActivity().getSharedPreferences("AUTHENTICATION", Context.MODE_PRIVATE);
+
+        logoutBtn.setOnClickListener(v->logout());
+
         return view;
+    }
+    private void logout() {
+        sharedPreferences.edit().remove("authToken").apply();
+        Toast.makeText(getContext(), "Logged Out Successfully", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(getContext(), SigninActivity.class));
+        requireActivity().finish();
     }
 }
