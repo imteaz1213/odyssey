2df23cdf4efd1dfaac5e62fa594d20d9cd9df5d2
masum184e <masum184e@gmail.com>
2024-11-24 13:23:49 +0600

    response integreated

--- app/src/main/java/com/example/odyssey/CarDetailsActivity.java
+++ app/src/main/java/com/example/odyssey/CarDetailsActivity.java
@@ -1,10 +1,12 @@
 package com.example.odyssey;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.odyssey.api.ApiService;
 import com.example.odyssey.api.RetrofitClient;
 import com.example.odyssey.models.VehicleResponse;
@@ -14,12 +16,13 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class CarDetailsActivity extends AppCompatActivity {
-
+    private ImageView main_car_image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_details);
 
+        main_car_image = findViewById(R.id.main_car_image);
 //        String carId = getIntent().getStringExtra("CAR_ID");
         String carId = "2";
         if (carId != null) {
@@ -42,6 +45,11 @@ public class CarDetailsActivity extends AppCompatActivity {
                         System.out.println("Hello");
                         System.out.println(vehicleResponse.getData().getVehicle_id());
                         Toast.makeText(CarDetailsActivity.this, vehicleResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                        Glide.with(CarDetailsActivity.this)
+                                .load(vehicleResponse.getData().getMain_image())
+                                .placeholder(R.drawable.car1)
+                                .error(R.drawable.car1)
+                                .into(main_car_image);
                     } else {
                         Toast.makeText(CarDetailsActivity.this, vehicleResponse.getMessage(), Toast.LENGTH_SHORT).show();
                     }
