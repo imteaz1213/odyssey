b112e6153102e68ca4ffa0adfca26d3512b69231
masum184e <masum184e@gmail.com>
2024-12-03 23:56:01 +0600

    actionbar added

--- app/src/main/java/com/example/odyssey/TakeLeaveActivity.java
+++ app/src/main/java/com/example/odyssey/TakeLeaveActivity.java
@@ -7,7 +7,9 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,14 +18,28 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.odyssey.databinding.ActivityTakeLeaveBinding;
 
 public class TakeLeaveActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    private TextView toolbarTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_leave);
 
+        toolbar = findViewById(R.id.toolbar);
+        toolbarTitle = findViewById(R.id.toolbar_title);
+        toolbarTitle.setText("Take Leave");
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
 
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        getOnBackPressedDispatcher().onBackPressed();
+        return true;
     }
 
 }
