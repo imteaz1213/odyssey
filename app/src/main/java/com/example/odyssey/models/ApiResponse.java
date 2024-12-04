dece239dd3aac0eab481c3c6b2e362a27c657711
masum184e <masum184e@gmail.com>
2024-12-04 14:04:54 +0600

    constructor added

--- app/src/main/java/com/example/odyssey/models/ApiResponse.java
+++ app/src/main/java/com/example/odyssey/models/ApiResponse.java
@@ -4,6 +4,11 @@ public class ApiResponse {
     private String status;
     private String message;
 
+    public ApiResponse(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
     public String getStatus() {
         return status;
     }
